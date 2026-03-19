b31a663d2448b334daa67c9983b0a40cafd2c9f5
Hamza <ghafarhamzaamsn.777@gmail.com>
2026-03-19 16:08:35 +0300

    Add app_assets.dart for centralized asset management

---
+++ lib/Core/utils/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAppIcon
+  /// assets/images/App_icon.png
+  static const String assetsImagesAppIcon = "assets/images/App_icon.png";
+
+  /// Assets for assetsImagesCircels
+  /// assets/images/Circels.svg
+  static const String assetsImagesCircels = "assets/images/Circels.svg";
+
+  /// Assets for assetsImagesFruit basket
+  /// assets/images/fruit basket.svg
+  static const String assetsImagesFruitbasket = "assets/images/fruit basket.svg";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/Logo.svg
+  static const String assetsImagesLogo = "assets/images/Logo.svg";
+
+  /// Assets for assetsImagesPineapple
+  /// assets/images/pineapple.svg
+  static const String assetsImagesPineapple = "assets/images/pineapple.svg";
+
+  /// Assets for assetsImagesPlant
+  /// assets/images/Plant.svg
+  static const String assetsImagesPlant = "assets/images/Plant.svg";
+
+  /// Assets for assetsImagesVector
+  /// assets/images/Vector.svg
+  static const String assetsImagesVector = "assets/images/Vector.svg";
+}
+
