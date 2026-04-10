8f35081ac5f8b46f4c84c1b9561526a4bda1c9e7
Hamza <ghafarhamzaamsn.777@gmail.com>
2026-04-10 16:48:15 +0300

    Add detailed error logging and handling for Firebase account creation

--- lib/Core/Services/firebase_auth_service.dart
+++ lib/Core/Services/firebase_auth_service.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../../../core/errors/exceptions.dart'; // سأوضح لك محتواه بالأسفل
@@ -13,6 +15,17 @@ class FirebaseAuthService {
           .createUserWithEmailAndPassword(email: email, password: password);
       return credential.user!;
     } on FirebaseAuthException catch (e) {
+      log("FirebaseAuthException: e.code : ${e.code} - ${e.message}");
+      if (e.code == 'network-request-failed') {
+        throw CustomException(
+          'لا يوجد اتصال بالإنترنت. يرجى التحقق من اتصالك والمحاولة مرة أخرى.',
+        );
+      }
+      if (e.code == 'invalid-email') {
+        throw CustomException('البريد الإلكتروني غير صالح');
+      } else if (e.code == 'operation-not-allowed') {
+        throw CustomException('عملية إنشاء الحساب غير مسموح بها');
+      }
       if (e.code == 'weak-password') {
         throw CustomException('كلمة السر ضعيفة جدا ');
       } else if (e.code == 'email-already-in-use') {
